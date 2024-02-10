84003163a077ffea2f81a0d92548e53c15df716c
Youness KOUROU <91493780+yokourou@users.noreply.github.com>
2024-02-10 17:23:03 +0100

    Delete requirements.py

--- requirements.py
+++
@@ -1,9 +0,0 @@
-
-import subprocess
-
-# Exécute la commande d'installation de Django en utilisant pip
-subprocess.call(['pip', 'install', 'django'])
-subprocess.call(['pip', 'install', 'reportlab'])
-subprocess.call(['pip', 'install', 'xhtml2pdf'])
-subprocess.call(['pip', 'install', 'tqdm'])
-subprocess.call(['pip', 'install', 'gunicorn'])
